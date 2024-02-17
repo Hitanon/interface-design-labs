85ea1bd147bd9f9c07c19dd6de5e2353c810918e
smwatertd <=>
2024-02-18 01:40:09 +0500

    add CategoryClient

---
+++ src/clients/CategoryClient.js
@@ -0,0 +1,14 @@
+import host from ".";
+
+export const getTopCategories = async (limit) => {
+  return [
+    {
+      id: 1,
+      name: "category1",
+    },
+    {
+      id: 2,
+      name: "category2",
+    },
+  ];
+};
