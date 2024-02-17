c57281f6d567431781fba7753a6c023c9c9d902f
smwatertd <=>
2024-02-18 02:30:09 +0500

    delete unused setters in ProjectInfoStore

--- src/store/ProjectInfoStore.js
+++ src/store/ProjectInfoStore.js
@@ -9,42 +9,26 @@ export default class ProjectInfoStore {
     makeAutoObservable(this);
   }
 
-  setPhone(phone) {
-    this._phone = phone;
+  setInfo(info) {
+    this.setPhone(info.phone);
+    this.setEmail(info.email);
+    this.setAbout(info.about);
+    this.setSocial(info.social);
   }
 
   get phone() {
     return this._phone;
   }
 
-  setEmail(email) {
-    this._email = email;
-  }
-
   get email() {
     return this._email;
-  }
-
-  setAbout(about) {
-    this._about = about;
   }
 
   get about() {
     return this._about;
   }
 
-  setSocial(social) {
-    this._social = social;
-  }
-
   get social() {
     return this._social;
-  }
-
-  setInfo(info) {
-    this.setPhone(info.phone);
-    this.setEmail(info.email);
-    this.setAbout(info.about);
-    this.setSocial(info.social);
   }
 }
