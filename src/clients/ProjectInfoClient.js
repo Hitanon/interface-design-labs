905ea900828d1dc8c4b7aa51a431e6a91af8a543
smwatertd <=>
2024-02-18 01:40:55 +0500

    add ProjectInfoClient

---
+++ src/clients/ProjectInfoClient.js
@@ -0,0 +1,15 @@
+import $host from ".";
+
+
+export const getProjectInfo = async () => {
+  return {
+    "phone": "+7 (999) 999-99-99",
+    "email": "9d0lQ@example.com",
+    "about": "about us",
+    "social": {
+      "facebook": "https://www.facebook.com/",
+      "instagram": "https://www.instagram.com/",
+      "youtube": "https://www.youtube.com/",
+    },
+  };
+};
