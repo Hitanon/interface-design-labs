1c57cf2f49c992b43bac58db84b63c2f3c6bef0f
smwatertd <=>
2024-02-18 01:42:38 +0500

    add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import ListButton from "./ui/ListButton";
+import SearchBar from "./ui/SearchBar";
+
+import { CATEGORIES_ROUTE, LIST_CATEGORIES_BUTTON_TEXT, SEARCH_PRODUCTS_BY_ALL_CATEGORIES_TEXT, SEARCH_PRODUCTS_BY_ALL_CATEGORIES_ROUTE } from "../utils/Consts";
+import { useNavigate } from "react-router-dom";
+
+
+export const Header = () => {
+  const navigate = useNavigate();
+
+  const onCategoriesClick = () => {
+    navigate(CATEGORIES_ROUTE);
+  };
+
+  const onSearchClick = () => {
+    navigate(SEARCH_PRODUCTS_BY_ALL_CATEGORIES_ROUTE);
+  };
+
+  return (
+    <>
+      <div>
+        Header
+      </div>
+      <ListButton text={LIST_CATEGORIES_BUTTON_TEXT} callback={onCategoriesClick} />
+      <SearchBar text={SEARCH_PRODUCTS_BY_ALL_CATEGORIES_TEXT} callback={onSearchClick} />
+    </>
+  );
+};
+
+export default Header;
