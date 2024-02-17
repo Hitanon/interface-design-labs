bd85afac721240d811f2adbf10be7b2653bcbe78
smwatertd <=>
2024-02-18 03:10:15 +0500

    add Product component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React, { createContext } from "react";
 import App from "./App";
 import TopItemsStore from "./store/TopItemsStore";
 import ProjectInfoStore from "./store/ProjectInfoStore";
+import ProductStore from "./store/ProductStore";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -13,6 +14,7 @@ root.render(
   <Context.Provider value={{
     topItems: new TopItemsStore(),
     projectInfo: new ProjectInfoStore(),
+    product: new ProductStore(),
   }}>
     <App />
   </Context.Provider>
---
+++ src/pages/Product.jsx
@@ -0,0 +1,40 @@
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import Footer from "../components/Footer";
+import FullProductInfo from "../components/FullProductInfo";
+import Header from "../components/Header";
+import { Context } from "..";
+import { getProjectInfo } from "../clients/ProjectInfoClient";
+import { getProduct } from "../clients/ProductClient";
+
+
+const Product = () => {
+  const { projectInfo, product } = useContext(Context);
+  const { id } = useParams();
+
+  const loadProjectInfo = async () => {
+    const info = await getProjectInfo();
+    projectInfo.setInfo(info);
+  };
+
+  const loadFullProductInfo = async () => {
+    const fullProductInfo = await getProduct(id);
+    product.setProduct(fullProductInfo);
+  };
+
+  useEffect(() => {
+    loadProjectInfo();
+    loadFullProductInfo();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <FullProductInfo />
+      <Footer />
+    </>
+  );
+};
+
+export default Product;
