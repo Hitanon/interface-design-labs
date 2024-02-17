468e4144b0be8c2d8576fe8a9ca8d8697362e949
smwatertd <=>
2024-02-18 01:40:43 +0500

    add ProductClient

---
+++ src/clients/ProductClient.js
@@ -0,0 +1,27 @@
+import $host from ".";
+
+
+export const getTopProducts = async (limit) => {
+  return [
+    {
+      id:1,
+      name: "product1",
+      price: 100,
+      unitsInStock: 10,
+      seller: {
+        id: 1,
+        name: "seller1",
+      },
+    },
+    {
+      id:2,
+      name: "product2",
+      price: 200,
+      unitsInStock: 20,
+      seller: {
+        id: 2,
+        name: "seller2",
+      },
+    },
+  ];
+};
