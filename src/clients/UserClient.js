d5563b75eacc5ca9fea101d965c472c776b52836
smwatertd <=>
2024-02-22 22:16:28 +0500

    add getUserOrders UserClient function

---
+++ src/clients/UserClient.js
@@ -0,0 +1,55 @@
+export const getUserOrders = async () => {
+  return [
+    {
+      id: 1,
+      createdAt: "2020-01-01T10:00:00",
+      items: [
+        {
+          id: 1,
+          statuses: [
+            {
+              id: 1,
+              name: "Status1",
+              startedAt: "2020-01-01T10:00:00",
+              endedAt: "2020-01-01T12:00:00",
+            },
+            {
+              id: 2,
+              name: "Status2",
+              startedAt: "2020-01-02T10:00:00",
+              endedAt: null,
+            },
+          ],
+        },
+        {
+          id: 2,
+          statuses: [
+            {
+              id: 1,
+              name: "Status1",
+              startedAt: "2020-01-01T10:00:00",
+              endedAt: null,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      id: 2,
+      createdAt: "2020-01-02T10:00:00",
+      items: [
+        {
+          id: 1,
+          statuses: [
+            {
+              id: 1,
+              name: "Status1",
+              startedAt: "2020-01-01T10:00:00",
+              endedAt: null,
+            },
+          ],
+        },
+      ],
+    },
+  ];
+};
