2c9f4f5207787982c68f15bceafb732e10cf5332
smwatertd <=>
2024-02-22 22:48:12 +0500

    add TopCategories component click handler

--- src/components/TopCategories.jsx
+++ src/components/TopCategories.jsx
@@ -1,13 +1,22 @@
 import { useContext } from "react";
 import { observer } from "mobx-react-lite";
+import { useNavigate } from "react-router-dom";
 
 import { Context } from "..";
+
+import { CATEGORIES_ROUTE } from "../utils/Consts";
 
 import Category from "./Category";
 
 
 const TopCategories = observer(() => {
   const { topItems } = useContext(Context);
+
+  const navigate = useNavigate();
+
+  const onCategoryClick = (categoryId) => {
+    navigate(`${CATEGORIES_ROUTE}/${categoryId}`);
+  };
 
   return (
     <>
@@ -16,7 +25,10 @@ const TopCategories = observer(() => {
       </div>
       <ul>
         {
-          topItems.categories.map((category) => <li key={category.id}><Category category={category} /></li>)
+          topItems.categories.map(
+            (category) =>
+              <li key={category.id} onClick={() => onCategoryClick(category.id)}><Category category={category} /></li>
+          )
         }
       </ul>
     </>
