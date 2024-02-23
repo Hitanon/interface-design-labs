ec610058220098934d7718051e61bdab0262c154
smwatertd <=>
2024-02-24 00:30:11 +0500

    add SellerInfo component

---
+++ src/components/SellerInfo.jsx
@@ -0,0 +1,14 @@
+import SellerHeader from "./SellerHeader";
+import SellerProducts from "./SellerProducts";
+
+
+const SellerInfo = () => {
+  return (
+    <>
+      <SellerHeader />
+      <SellerProducts />
+    </>
+  );
+};
+
+export default SellerInfo;
