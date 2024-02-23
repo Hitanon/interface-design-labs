a1e4021c04b88467ec53e0dad9266edf943096bb
smwatertd <=>
2024-02-23 14:20:14 +0500

    add getAllCategories CategoryClient function

--- src/clients/CategoryClient.js
+++ src/clients/CategoryClient.js
@@ -10,3 +10,28 @@ export const getTopCategories = async (limit) => {
     },
   ];
 };
+
+export const getAllCategories = async () => {
+  return [
+    {
+      id: 1,
+      name: "category1",
+    },
+    {
+      id: 2,
+      name: "category2",
+    },
+    {
+      id: 3,
+      name: "category3",
+    },
+    {
+      id: 4,
+      name: "category4",
+    },
+    {
+      id: 5,
+      name: "category5",
+    },
+  ];
+};
