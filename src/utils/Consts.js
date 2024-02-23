19230a1c07aa915a9c50460265724a0cb6df4d90
smwatertd <=>
2024-02-23 23:03:41 +0500

    add users features routes

--- src/utils/Consts.js
+++ src/utils/Consts.js
@@ -5,10 +5,14 @@ export const PRODUCTS_ROUTE = "/products";
 export const SEARCH_PRODUCTS_BY_ALL_CATEGORIES_ROUTE = `${PRODUCTS_ROUTE}/search`;
 export const PRODUCT_ROUTE = `${PRODUCTS_ROUTE}/:id`;
 export const PROFILE_ROUTE = "/profile";
+export const LOGIN_ROUTE = "/login";
+export const LOGOUT_ROUTE = "/logout";
+export const REGISTRATION_ROUTE = "/registration";
 
 // Settings
 export const DEFAULT_TOP_CATEGORIES_COUNT = 5;
 export const DEFAULT_TOP_ITEMS_COUNT = 5;
 
+// UI
 export const LIST_CATEGORIES_BUTTON_TEXT = "Категории";
 export const SEARCH_PRODUCTS_BY_ALL_CATEGORIES_TEXT = "Поиск по всем категориям";
