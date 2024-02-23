c2bfb21e06aaf3159515987794f7ebe615c1f2a2
smwatertd <=>
2024-02-23 23:01:58 +0500

    add authHost

--- src/clients/index.js
+++ src/clients/index.js
@@ -4,4 +4,21 @@ const $host = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
+const $authHost = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+const authHostConfig = (config) => {
+  config.headers.Authorization = `Token ${getToken()}`;
+  return config;
+};
+
+const getToken = () => {
+  const token = localStorage.getItem("token");
+  return token === null ? "" : token;
+};
+
+$authHost.interceptors.request.use(authHostConfig);
+
 export default $host;
+export { $authHost };
