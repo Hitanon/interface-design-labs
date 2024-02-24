4976579cb987b4c10920c3c05b1ad136cbafea30
smwatertd <=>
2024-02-24 21:39:49 +0500

    update getAllCategories function

--- src/clients/CategoryClient.js
+++ src/clients/CategoryClient.js
@@ -7,26 +7,6 @@ export const getTopCategories = async (limit) => {
 };
 
 export const getAllCategories = async () => {
-  return [
-    {
-      id: 1,
-      name: "category1",
-    },
-    {
-      id: 2,
-      name: "category2",
-    },
-    {
-      id: 3,
-      name: "category3",
-    },
-    {
-      id: 4,
-      name: "category4",
-    },
-    {
-      id: 5,
-      name: "category5",
-    },
-  ];
+  const response = await $host.get("/api/categories");
+  return response.data;
 };
