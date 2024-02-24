285543df710729737d23eeb4115f50a8c49d6ca4
smwatertd <=>
2024-02-24 13:44:43 +0500

    add getSellerOrder SellerClient function

--- src/clients/SellerClient.js
+++ src/clients/SellerClient.js
@@ -55,3 +55,47 @@ export const getSellerOrders = () => {
 
 export const moveToNextStatus = async (id) => {
 };
+
+export const getSellerOrder = async (id) => {
+  return {
+    id: 1,
+    createdAt: "2020-01-01T10:00:00",
+    items: [
+      {
+        id: 1,
+        statuses: [
+          {
+            id: 1,
+            name: "Status1",
+            startedAt: "2020-01-01T10:00:00",
+            endedAt: "2020-01-01T12:00:00",
+          },
+          {
+            id: 2,
+            name: "Status2",
+            startedAt: "2020-01-02T10:00:00",
+            endedAt: "2020-01-02T12:00:00",
+          },
+          {
+            id: 3,
+            name: "Status3",
+            startedAt: "2020-01-02T10:00:00",
+            endedAt: null,
+          },
+        ],
+      },
+      {
+        id: 2,
+        createdAt: "2020-01-02T10:00:00",
+        statuses: [
+          {
+            id: 1,
+            name: "Status1",
+            startedAt: "2020-01-01T10:00:00",
+            endedAt: null,
+          },
+        ],
+      },
+    ],
+  };
+};
