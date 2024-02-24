fd788c2fea99f643de57d39b821268895a929ec2
smwatertd <=>
2024-02-24 13:47:13 +0500

    temporary replacement customer profile to seller profile in profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,19 +1,53 @@
 import { useContext, useEffect } from "react";
 
 import { Context } from "..";
-import { getUserOrders } from "../clients/UserClient";
-import CustomerOrders from "../components/CustomerOrders";
+// import { getUserOrders } from "../clients/UserClient";
+// import CustomerOrders from "../components/CustomerOrders";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { getProjectInfo } from "../clients/ProjectInfoClient";
+import SellerOrders from "../components/SellerOrders";
+import { getSellerOrders } from "../clients/SellerClient";
+
+
+// const Profile = () => {
+//   const { user, projectInfo } = useContext(Context);
+
+//   const loadOrders = async () => {
+//     const orders = await getUserOrders();
+//     user.setOrders(orders);
+//   };
+
+//   const loadProjectInfo = async () => {
+//     const info = await getProjectInfo();
+//     projectInfo.setInfo(info);
+//   };
+
+//   useEffect(() => {
+//     loadOrders();
+//     loadProjectInfo();
+//   }, []);
+
+//   return (
+//     <>
+//       <Header />
+//       <hr />
+//       <CustomerOrders />
+//       <hr />
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default Profile;
 
 
 const Profile = () => {
-  const { user, projectInfo } = useContext(Context);
+  const { sellerOrders, projectInfo } = useContext(Context);
 
-  const loadOrders = async () => {
-    const orders = await getUserOrders();
-    user.setOrders(orders);
+  const loadSellerOrders = async () => {
+    const orders = await getSellerOrders();
+    sellerOrders.setOrders(orders);
   };
 
   const loadProjectInfo = async () => {
@@ -22,7 +56,7 @@ const Profile = () => {
   };
 
   useEffect(() => {
-    loadOrders();
+    loadSellerOrders();
     loadProjectInfo();
   }, []);
 
@@ -30,7 +64,7 @@ const Profile = () => {
     <>
       <Header />
       <hr />
-      <CustomerOrders />
+      <SellerOrders />
       <hr />
       <Footer />
     </>
