0238d5d135e89cb875da1d227c46ed0872d10762
smwatertd <=>
2024-02-24 21:41:55 +0500

    update getTopProducts

--- src/clients/ProductClient.js
+++ src/clients/ProductClient.js
@@ -1,3 +1,6 @@
+import { $host } from ".";
+
+
 const products = [
   {
     id: 1,
@@ -23,61 +26,8 @@ const products = [
 
 
 export const getTopProducts = async (limit) => {
-  return products;
-};
-
-export const getProduct = async (id) => {
-  return {
-    id: 1,
-    images: ["https://example.com/image1.jpg", "https://example.com/image2.jpg"],
-    name: "product1",
-    description: "description",
-    price: 100,
-    rating: 5,
-    unitsInStock: 10,
-    category: {
-      id: 1,
-      name: "category1",
-    },
-    seller: {
-      id: 1,
-      name: "seller1",
-    },
-    options: [
-      {
-        id: 1,
-        name: "option1",
-        values: [
-          {
-            id: 1,
-            name: "value1",
-          },
-          {
-            id: 2,
-            name: "value2",
-          },
-        ],
-      },
-    ],
-    comments: [
-      {
-        rating: 4,
-        text: "text1",
-        author: {
-          id: 1,
-          name: "author1",
-        },
-      },
-      {
-        rating: 5,
-        text: "text2",
-        author: {
-          id: 2,
-          name: "author2",
-        },
-      },
-    ],
-  };
+  const response = await $host.get("/api/products/top");
+  return response.data;
 };
 
 
