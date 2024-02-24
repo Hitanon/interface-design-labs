b68e541e71442d5b781fe1fbba1ac7abadebfe00
smwatertd <=>
2024-02-24 14:35:58 +0500

    add addToCart CustomerClient function

---
+++ src/clients/CustomerClient.js
@@ -0,0 +1,7 @@
+const cart = [];
+
+export const addToCart = async (id, quantity) => {
+  cart.findIndex(item => item.id === id) === -1
+    ? cart.push({id, quantity})
+    : cart[cart.findIndex(item => item.id === id)].quantity += quantity;
+};
