7b68a48270e1d02f5c4ed00f2308f9864a153e8e
smwatertd <=>
2024-03-02 22:54:54 +0500

    refactor Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,33 +7,21 @@ import {
   LIST_CATEGORIES_BUTTON_TEXT,
   SEARCH_PRODUCTS_BY_ALL_CATEGORIES_TEXT,
   SEARCH_PRODUCTS_BY_ALL_CATEGORIES_ROUTE,
-  PROFILE_ROUTE,
   MAIN_ROUTE,
-  LOGIN_ROUTE,
-  REGISTRATION_ROUTE,
-  PLACE_AN_ORDER_BUTTON_TEXT,
-  CART_ROUTE,
 } from "../utils/Consts";
 import { Context } from "..";
-import { logout } from "../clients/UserClient";
 
 import ListButton from "./ui/ListButton";
 import SearchBar from "./ui/SearchBar";
-import Cart from "./Cart";
+import UserBar from "./UserBar";
+import AuthenticateBar from "./AuthenticateBar";
+import ImageButton from "./ui/ImageButton";
 
 
 const Header = observer(() => {
   const { user } = useContext(Context);
 
   const navigate = useNavigate();
-
-  const onLogoClick = () => {
-    navigate(MAIN_ROUTE);
-  };
-
-  const onProfileClick = () => {
-    navigate(PROFILE_ROUTE);
-  };
 
   const onCategoriesClick = () => {
     navigate(CATEGORIES_ROUTE);
@@ -43,50 +31,16 @@ const Header = observer(() => {
     navigate(SEARCH_PRODUCTS_BY_ALL_CATEGORIES_ROUTE);
   };
 
-  const onLoginClick = () => {
-    navigate(LOGIN_ROUTE);
-  };
-
-  const onRegisterClick = () => {
-    navigate(REGISTRATION_ROUTE);
-  };
-
-  const onPlaceAnOrderClick = () => {
-    navigate(CART_ROUTE);
-  };
-
-  const onLogoutClick = async () => {
-    user.setIsAuth(false);
-    user.setUsername("");
-    await logout();
-    localStorage.removeItem("token");
-  };
-
   return (
     <>
-      <div onClick={onLogoClick}>Logo</div>
-      <button onClick={onProfileClick}>Profile</button>
+      <ImageButton src="img/logo.png" alt="logo" route={MAIN_ROUTE} />
       <ListButton text={LIST_CATEGORIES_BUTTON_TEXT} callback={onCategoriesClick} />
       <SearchBar text={SEARCH_PRODUCTS_BY_ALL_CATEGORIES_TEXT} callback={onSearchClick} />
-
       {
         user.isAuth
-          ?
-          <div>
-            <div>
-              Username: {user.username}
-            </div>
-            <button onClick={onLogoutClick}>Logout</button>
-          </div>
-          :
-          <div>
-            <button onClick={onLoginClick}>Login</button>
-            <button onClick={onRegisterClick}>Register</button>
-          </div>
+          ? <UserBar />
+          : <AuthenticateBar />
       }
-
-      <Cart />
-      <button onClick={onPlaceAnOrderClick}>{PLACE_AN_ORDER_BUTTON_TEXT}</button>
     </>
   );
 });
