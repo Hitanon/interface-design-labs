cf051747d7b6bf28a725d78a60c38c5773d410b4
smwatertd <=>
2024-03-02 22:54:20 +0500

    add AuthenticateBar component

---
+++ src/components/AuthenticateBar.jsx
@@ -0,0 +1,15 @@
+import { LOGIN_BUTTON_TEXT, LOGIN_ROUTE, REGISTRATION_BUTTON_TEXT, REGISTRATION_ROUTE } from "../utils/Consts";
+
+import TextRedirectButton from "./ui/TextRedirectButton";
+
+
+const AuthenticateBar = () => {
+  return (
+    <>
+      <TextRedirectButton text={LOGIN_BUTTON_TEXT} route={LOGIN_ROUTE} />
+      <TextRedirectButton text={REGISTRATION_BUTTON_TEXT} route={REGISTRATION_ROUTE} />
+    </>
+  );
+};
+
+export default AuthenticateBar;
