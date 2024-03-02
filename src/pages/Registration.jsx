b0311408a6bf259906b1c340be819b51e7bd2277
smwatertd <=>
2024-03-02 22:56:20 +0500

    refactor register page

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,71 +1,45 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import { Context } from "..";
-import { login, register } from "../clients/UserClient";
-import { LOGIN_ROUTE, MAIN_ROUTE } from "../utils/Consts";
+import {
+  HAVE_AN_ACCOUNT_BUTTON_TEXT,
+  LOGIN_ROUTE, MAIN_ROUTE,
+  REGISTRATION_BUTTON_TEXT,
+  REGISTRATION_FORM_TITLE,
+} from "../utils/Consts";
+import useAuthenticate from "../hooks/useAuthenticate";
+import TextRedirectButton from "../components/ui/TextRedirectButton";
+import TextButton from "../components/ui/TextButton";
+import InputField from "../components/ui/InputField";
 
 
 const Registration = () => {
-  const { user } = useContext(Context);
-
-  const [ credentials, setCredentials ] = useState({
-    email: "admin@test.com",
-    username: "admin",
-    password: "secret",
-  });
-
+  const [email, setEmail] = useState("admin@test.com");
+  const [username, setUsername] = useState("admin");
+  const [password, setPassword] = useState("secret");
   const navigate = useNavigate();
+  const { register } = useAuthenticate();
+
 
   const onRegisterClick = async () => {
-    await register(credentials);
-    const response = await login({email: credentials.email, password: credentials.password});
-    setAuthToken(response.token);
-    authorizeUser(response.username);
+    await register({email, username, password});
     navigate(MAIN_ROUTE);
-  };
-
-  const onHaveAccount = () => {
-    navigate(LOGIN_ROUTE);
-  };
-
-  const setAuthToken = (token) => {
-    localStorage.setItem("token", token);
-  };
-
-  const authorizeUser = (username) => {
-    user.setUsername(username);
-    user.setIsAuth(true);
   };
 
   return (
     <>
       <div>
-        Email:
-        <input
-          type="email"
-          value={credentials.email}
-          onChange={(e) => setCredentials({...credentials, email: e.target.value})}
-        />
+        {REGISTRATION_FORM_TITLE}
       </div>
       <div>
-        Username:
-        <input
-          type="text"
-          value={credentials.username}
-          onChange={(e) => setCredentials({...credentials, username: e.target.value})}
-        />
+        <InputField type="email" value={email} callback={setEmail}/>
+        <InputField type="text" value={username} callback={setUsername}/>
+        <InputField type="password" value={password} callback={setPassword}/>
       </div>
       <div>
-        Password:
-        <input
-          type="password"
-          value={credentials.password}
-          onChange={(e) => setCredentials({...credentials, password: e.target.value})}
-        />
+        <TextButton text={REGISTRATION_BUTTON_TEXT} callback={onRegisterClick}/>
+        <TextRedirectButton text={HAVE_AN_ACCOUNT_BUTTON_TEXT} route={LOGIN_ROUTE} />
       </div>
-      <button onClick={onRegisterClick}>Register</button>
-      <button onClick={onHaveAccount}>Have an account?</button>
     </>
   );
 };
