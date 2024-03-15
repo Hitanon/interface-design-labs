58f1d1f067a7d0501950aa328c6f0dc85aa11a98
Danil Korablev <=>
2024-03-15 21:19:53 +0500

    add CreateCommentForm component

---
+++ src/components/forms/CreateCommentForm.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+import InputField from "../ui/InputField";
+import TextButton from "../ui/TextButton";
+import { CREATE_COMMENT_BUTTON_TEXT } from "../../utils/Consts";
+import useComments from "../../hooks/useComments";
+
+
+const CreateCommentForm = () => {
+  const { createComment } = useComments();
+
+  const [rating, setRating] = useState(0);
+  const [message, setMessage] = useState("");
+
+  const createProductComment = async () => {
+    await createComment(rating, message);
+  };
+
+  return (
+    <>
+      <div>
+        <InputField type="number" placeholder="Rating" value={rating} callback={setRating} />
+        <InputField type="text" placeholder="Message" value={message} callback={setMessage} />
+      </div>
+      <div>
+        <TextButton text={CREATE_COMMENT_BUTTON_TEXT} callback={createProductComment} />
+      </div>
+    </>
+  );
+};
+
+export default CreateCommentForm;
