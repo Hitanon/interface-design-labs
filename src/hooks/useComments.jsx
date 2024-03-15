0eafe559e3aeefdca836fe3282f24c3b5233c4aa
Danil Korablev <=>
2024-03-15 21:19:15 +0500

    add useComments hook

---
+++ src/hooks/useComments.jsx
@@ -0,0 +1,44 @@
+import { useContext } from "react";
+
+import { Context } from "..";
+import { createProductComment, updateProductComment } from "../clients/CustomerClient";
+import { getProduct } from "../clients/ProductClient";
+
+
+const useComments = () => {
+  const { user, product } = useContext(Context);
+
+  const isUserCommentExists = () => {
+    const usernames = product.comments.map(comment => comment.author);
+    return usernames.includes(user.username);
+  };
+
+  const getUserComment = () => {
+    if (isUserCommentExists()) {
+      return product.comments.filter(comment => comment.author === user.username)[0];
+    }
+  };
+
+  const updateProduct = async () => {
+    product.setProduct(await getProduct(product.id));
+  };
+
+  const updateComment = async (id, rating, message) => {
+    await updateProductComment(id, rating, message);
+    await updateProduct();
+  };
+
+  const createComment = async (rating, message) => {
+    await createProductComment(rating, message);
+    await updateProduct();
+  };
+
+  return {
+    isUserCommentExists,
+    getUserComment,
+    updateComment,
+    createComment,
+  };
+};
+
+export default useComments;
