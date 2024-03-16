0b277befa7a035f5c96df4f563f83f30700f7edb
Danil Korablev <=>
2024-03-16 17:42:40 +0500

    add getCategory function

--- src/clients/CategoryClient.js
+++ src/clients/CategoryClient.js
@@ -1,6 +1,11 @@
 import { $host } from "./index";
 
 
+export const getCategory = async (id) => {
+  const response = await $host.get(`/api/categories/${id}`);
+  return response.data;
+};
+
 export const getTopCategories = async (limit) => {
   const response = await $host.get("/api/categories/top");
   return response.data;
