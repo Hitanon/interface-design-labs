308c33a87312aba0621576960dccb42aaa52bb6c
Danil Korablev <=>
2024-03-22 17:07:26 +0500

    refactor FullProductInfo with loading date in self

--- src/components/FullProductInfo.jsx
+++ src/components/FullProductInfo.jsx
@@ -1,48 +1,64 @@
-import { useContext } from "react";
-import { observer } from "mobx-react-lite";
+import { useEffect, useState } from "react";
 
-import { Context } from "..";
+import { getProduct } from "../clients/ProductClient";
 
 import ProductComments from "./ProductComments";
-// import ProductImageSlider from "./ProductImageSlider";
 import ProductOptions from "./ProductOptions";
 import AddToCartButton from "./ui/AddToCartButton";
 import UserComment from "./UserComment";
+import ImageSlider from "./ui/ImageSlider";
+import SellerButton from "./sellers/SellerButton";
+import CategoryButton from "./categories/CategoryButton";
 
 
-const FullProductInfo = observer(() => {
-  const { product } = useContext(Context);
+const FullProductInfo = ({ id }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [product, setProduct] = useState({});
+
+  const loadFullProductInfo = async () => {
+    setIsLoading(true);
+    setProduct(await getProduct(id));
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    loadFullProductInfo();
+  }, []);
+
+  // TODO: refactor. Make it better
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
-      {/* <ProductImageSlider /> */}
       <div>
-        { product.name }
+        <ImageSlider />
+        <div>
+          Name: {product.name}
+        </div>
+        <div>
+          Description: {product.description}
+        </div>
+        <div>
+          Price: {product.price}
+        </div>
+        <div>
+          Rating: {product.rating}
+        </div>
+        <div>
+          Unit in Stock: {product.unitsInStock}
+        </div>
+        <CategoryButton id={product.category.id} />
+        <SellerButton id={product.seller.id} />
+        <ProductOptions />
+        <UserComment />
+        <ProductComments />
+        <hr />
+        <AddToCartButton productId={product.id} />
       </div>
-      <div>
-        { product.description }
-      </div>
-      <div>
-        { product.price }
-      </div>
-      <div>
-        { product.rating }
-      </div>
-      <div>
-        { product.unitsInStock }
-      </div>
-      <div>
-        { product.category.name }
-      </div>
-      <div>
-        { product.seller.name }
-      </div>
-      <ProductOptions />
-      <UserComment />
-      <ProductComments />
-      <AddToCartButton productId={product.id} />
     </>
   );
-});
+};
 
 export default FullProductInfo;
