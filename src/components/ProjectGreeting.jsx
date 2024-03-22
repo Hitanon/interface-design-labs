6b0e52c3e6ec9e3bc1e93931581152955daf0167
Danil Korablev <=>
2024-03-22 12:32:21 +0500

    add categories button on project greeting component

--- src/components/ProjectGreeting.jsx
+++ src/components/ProjectGreeting.jsx
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
 import { observer } from "mobx-react-lite";
 
+import { CATEGORIES_ROUTE, LIST_CATEGORIES_BUTTON_TEXT } from "../utils/Consts";
 import useProjectInfo from "../hooks/useProjectInfo";
+
+import TextRedirectButton from "./ui/TextRedirectButton";
 
 
 const ProjectGreeting = observer(() => {
@@ -18,6 +21,9 @@ const ProjectGreeting = observer(() => {
       </div>
       <div>
         {projectInfo.greeting}
+      </div>
+      <div>
+        <TextRedirectButton text={LIST_CATEGORIES_BUTTON_TEXT} route={CATEGORIES_ROUTE} />
       </div>
     </>
   );
