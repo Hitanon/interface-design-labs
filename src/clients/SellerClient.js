e6cfd7188b4b1a512d2902832c7bc0b28b6cfbd0
Danil Korablev <=>
2024-03-22 16:35:46 +0500

    add get seller info function implementation

--- src/clients/SellerClient.js
+++ src/clients/SellerClient.js
@@ -1,16 +1,8 @@
-import { products } from "./ProductClient";
-
+import { $host } from ".";
 
 export const getSellerInfo = async (id) => {
-  return {
-    id: 1,
-    name: "seller1",
-    description: "description",
-    rating: 5,
-    sales: 100,
-    sellsFrom: "2020-01-01T10:00:00",
-    products: products,
-  };
+  const response = await $host.get(`/api/sellers/${id}`);
+  return response.data;
 };
 
 export const getSellerOrders = () => {
