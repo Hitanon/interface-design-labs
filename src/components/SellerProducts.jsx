93b271c5a5c89071b79e4259cccb69840a97df9e
Danil Korablev <=>
2024-03-22 16:48:19 +0500

    refactor SellerProduct component with ProductCard

--- src/components/SellerProducts.jsx
+++ src/components/SellerProducts.jsx
@@ -1,31 +1,20 @@
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 
 import { Context } from "..";
-import { PRODUCTS_ROUTE } from "../utils/Consts";
 
-import ShortProductInfo from "./ShortProductInfo";
+import ProductCard from "./products/ProductCard";
 
 
 const SellerProducts = observer(() => {
-  const navigate = useNavigate();
-
   const { seller } = useContext(Context);
-
-  const onProductClick = async (productId) => {
-    navigate(`${PRODUCTS_ROUTE}/${productId}`);
-  };
 
   return (
     <>
       Products
-      <ul>
-        {seller.products.map(
-          product =>
-            <li key={product.id} onClick={() => onProductClick(product.id)}><ShortProductInfo product={product} /></li>
-        )}
-      </ul>
+      {
+        seller.products.map(product => <div key={product.id}><ProductCard id={product.id} /></div>)
+      }
     </>
   );
 });
