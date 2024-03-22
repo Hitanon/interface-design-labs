778602e9e76f66f916f8321df669131e2c793284
Danil Korablev <=>
2024-03-22 16:57:29 +0500

    delete Products component observer

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,17 +1,14 @@
-import { observer } from "mobx-react-lite";
-
 import ProductCard from "./products/ProductCard";
 
 
-const Products = observer(({ products }) => {
+const Products = ({ products }) => {
   return (
     <>
-      Products
       {
         products.map(product => <div key={product.id}><ProductCard id={product.id} /></div>)
       }
     </>
   );
-});
+};
 
 export default Products;
