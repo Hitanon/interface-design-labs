679838865821f0611416c1d808ae9adf920f268a
Danil Korablev <=>
2024-03-22 13:11:34 +0500

    add general page components to login page

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -11,7 +11,7 @@ import {
   DOESNT_HAVE_AN_ACCOUNT_BUTTON_TEXT,
   LOGIN_BUTTON_TEXT,
   REGISTRATION_ROUTE,
-} from "../utils/Consts";
+} from "../../utils/Consts";
 
 
 const LoginForm = () => {
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,20 @@
 import { LOGIN_FORM_TITLE } from "../utils/Consts";
 import LoginForm from "../components/forms/LoginForm";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 
 const Login = () => {
   return (
     <>
+      <Header />
+      <hr />
       <div>
         {LOGIN_FORM_TITLE}
       </div>
       <LoginForm />
+      <hr />
+      <Footer />
     </>
   );
 };
