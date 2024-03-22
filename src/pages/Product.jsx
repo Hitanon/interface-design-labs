cf86dd3bafab9a1bf1c351766a0d8bc1fb74592b
Danil Korablev <=>
2024-03-22 16:49:16 +0500

    refactor Product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,31 +1,18 @@
-import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import Footer from "../components/Footer";
 import FullProductInfo from "../components/FullProductInfo";
 import Header from "../components/Header";
-import { Context } from "..";
-import { getProduct } from "../clients/ProductClient";
 
 
 const Product = () => {
-  const { product } = useContext(Context);
   const { id } = useParams();
-
-  const loadFullProductInfo = async () => {
-    const fullProductInfo = await getProduct(id);
-    product.setProduct(fullProductInfo);
-  };
-
-  useEffect(() => {
-    loadFullProductInfo();
-  }, []);
 
   return (
     <>
       <Header />
       <hr/>
-      <FullProductInfo />
+      <FullProductInfo id={id}/>
       <hr/>
       <Footer />
     </>
