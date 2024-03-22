b640955a3acf88691c8ec77368647714d08edc0d
Danil Korablev <=>
2024-03-22 12:47:16 +0500

    add CategoryCard component usage in Categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,31 +1,17 @@
-import { useNavigate } from "react-router-dom";
-
-import useSearch from "../hooks/useSearch";
-import { SEARCH_PRODUCTS_BY_ALL_CATEGORIES_ROUTE } from "../utils/Consts";
-
-import Category from "./Category";
+import CategoryCard from "./categories/CategoryCard";
 
 
 const Categories = ({ categories }) => {
-  const { search } = useSearch();
-  const navigate = useNavigate();
-
-  const onCategoryClick = (id) => {
-    search.addParam({ name: "category", value: id });
-    navigate(`${SEARCH_PRODUCTS_BY_ALL_CATEGORIES_ROUTE}`);
-  };
-
   return (
     <>
       <div>
         Categories
       </div>
-      <ul>
-        {categories.map(
-          (category) =>
-            <li onClick={() => onCategoryClick(category.id)} key={category.id}><Category category={category} /></li>
-        )}
-      </ul>
+      <div>
+        {
+          categories.map(category => <CategoryCard key={category.id} id={category.id} />)
+        }
+      </div>
     </>
   );
 };
