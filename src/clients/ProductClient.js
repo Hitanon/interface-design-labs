f42e9f85e145b0efea169fa3d1b070b78f79eb95
Danil Korablev <=>
2024-03-23 18:17:46 +0500

    delete products stub

--- src/clients/ProductClient.js
+++ src/clients/ProductClient.js
@@ -1,30 +1,6 @@
 import { $host } from ".";
 
 
-const products = [
-  {
-    id: 1,
-    name: "product1",
-    price: 100,
-    unitsInStock: 10,
-    seller: {
-      id: 1,
-      name: "seller1",
-    },
-  },
-  {
-    id: 2,
-    name: "product2",
-    price: 200,
-    unitsInStock: 20,
-    seller: {
-      id: 2,
-      name: "seller2",
-    },
-  },
-];
-
-
 export const getTopProducts = async (limit) => {
   const response = await $host.get("/api/products/top");
   return response.data;
@@ -40,5 +16,3 @@ export const searchProductsByParams = async (urlParams) => {
   const response = await $host.get(`/api/products${urlParams}`);
   return response.data;
 };
-
-export { products };
