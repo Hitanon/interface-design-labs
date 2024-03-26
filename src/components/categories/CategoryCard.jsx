ab187e579dfd7df89304fec11e75763405036760
Danil Korablev <=>
2024-03-26 18:21:55 +0500

    refactor CategoryCard

--- src/components/categories/CategoryCard.jsx
+++ src/components/categories/CategoryCard.jsx
@@ -1,29 +1,14 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-
 import ImageField from "../ui/ImageField";
-import { getCategory } from "../../clients/CategoryClient";
 import useSearch from "../../hooks/useSearch";
-import { SEARCH_PRODUCTS_ROUTE } from "../../utils/Consts";
 
 
-const CategoryCard = ({ id }) => {
-  const navigate = useNavigate();
-  const { search, getUrlParams } = useSearch();
-  const [category, setCategory] = useState({});
+const CategoryCard = ({ category }) => {
+  const { addParam, applyFilters } = useSearch();
 
   const onCategoryClick = () => {
-    search.addParam({ name: "category", value: id });
-    navigate(`${SEARCH_PRODUCTS_ROUTE}${getUrlParams()}`);
+    addParam("category", category.id);
+    applyFilters();
   };
-
-  const loadCategory = async () => {
-    setCategory(await getCategory(id));
-  };
-
-  useEffect(() => {
-    loadCategory();
-  }, []);
 
   return (
     <div className="category" onClick={onCategoryClick}>
