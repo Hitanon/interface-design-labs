1456e7d8197f52bc1b701ee936deb4a0fd36c5d7
Danil Korablev <=>
2024-03-26 18:40:30 +0500

    refactor Product

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -2,13 +2,13 @@ import { useContext, useEffect, useState } from "react";
 
 import AddToCartButton from "../ui/AddToCartButton";
 import UserComment from "../customers/UserComment";
-import ImageSlider from "../ui/ImageSlider";
 import SellerButton from "../sellers/SellerButton";
 import CategoryButton from "../categories/CategoryButton";
 import { Context } from "../..";
 import useProducts from "../../hooks/useProducts";
 
 import ProductComments from "./ProductComments";
+import ProductInfo from "./ProductInfo";
 
 
 const Product = ({ id }) => {
@@ -17,46 +17,28 @@ const Product = ({ id }) => {
   const [isLoading, setIsLoading] = useState(true);
 
   const loadProduct = async () => {
-    setIsLoading(true);
     product.setProduct(await get(id));
-    setIsLoading(false);
   };
 
   useEffect(() => {
+    setIsLoading(true);
     loadProduct();
+    setIsLoading(false);
   }, []);
 
-  // TODO: refactor. Make it better
   if (isLoading) {
     return <></>;
   }
 
   return (
     <>
-      <div>
-        <ImageSlider images={[]} />
-        <div>
-          Name: {product.name}
-        </div>
-        <div>
-          Description: {product.description}
-        </div>
-        <div>
-          Price: {product.price}
-        </div>
-        <div>
-          Rating: {product.rating}
-        </div>
-        <div>
-          Unit in Stock: {product.unitsInStock}
-        </div>
-        <CategoryButton category={product.category} />
-        <SellerButton id={product.seller.id} />
-        <UserComment />
-        <ProductComments comments={product.comments} />
-        <hr />
-        <AddToCartButton id={product.id} />
-      </div>
+      <ProductInfo product={product} />
+      <CategoryButton category={product.category} />
+      <SellerButton seller={product.seller} />
+      <UserComment />
+      <ProductComments comments={product.comments} />
+      <hr />
+      <AddToCartButton id={product.id} />
     </>
   );
 };
