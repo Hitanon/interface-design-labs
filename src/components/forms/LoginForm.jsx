ca7ef56eb3a510fe8d21fb7741df284d1aa363b5
Danil Korablev <=>
2024-04-06 23:06:27 +0500

    fix lint errors

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -43,9 +43,20 @@ const LoginForm = () => {
           {LOGIN_FORM_TITLE}
         </h2>
         <div>
-          <InputField className="input-auth" type="email" value={email} callback={setEmail} placeholder="E-mail" />
-          <InputField className="input-auth" type="password" value={password} callback={setPassword}
-            placeholder="Пароль" />
+          <InputField
+            className="input-auth"
+            type="email"
+            value={email}
+            callback={setEmail}
+            placeholder="E-mail"
+          />
+          <InputField
+            className="input-auth"
+            type="password"
+            value={password}
+            callback={setPassword}
+            placeholder="Пароль"
+          />
         </div>
         <div className="auth-buttons">
           <TextButton text={LOGIN_BUTTON_TEXT} callback={onLoginClick} />
