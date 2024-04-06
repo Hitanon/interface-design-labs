1b5ceca55bb03d109a516efd79233c96931256b9
Danil Korablev <=>
2024-04-06 23:23:40 +0500

    fix

--- src/components/products/ProductComments.jsx
+++ src/components/products/ProductComments.jsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Pagination } from "@mui/material";
+import { observer } from "mobx-react-lite";
 
 import { COMMENTS_PER_PAGE } from "../../utils/Consts";
+import { Context } from "../..";
 
 import ProductComment from "./ProductComment";
 
 
-const ProductComments = ({ comments }) => {
+const ProductComments = observer(() => {
+  const { product } = useContext(Context);
   const [page, setPage] = useState(1);
 
   const getCommentsCountText = () => {
+    const comments = product.comments;
     if (comments.length % 10 === 1) {
       return `${comments.length} отзыв о товаре`;
     }
@@ -22,8 +26,19 @@ const ProductComments = ({ comments }) => {
   const getPagedComments = () => {
     const startIndex = (page - 1) * COMMENTS_PER_PAGE;
     const endIndex = startIndex + COMMENTS_PER_PAGE;
-    return comments.slice(startIndex, endIndex);
+    return product.comments.slice(startIndex, endIndex);
   };
+
+  const checkPage = () => {
+    const temp = Math.ceil(product.comments.length / COMMENTS_PER_PAGE);
+    if (page > temp) {
+      setPage(temp);
+    }
+  };
+
+  useEffect(() => {
+    checkPage();
+  }, [product.comments]);
 
   return (
     <>
@@ -34,10 +49,10 @@ const ProductComments = ({ comments }) => {
         {getPagedComments().map((comment) => <ProductComment key={comment.id} comment={comment} />)}
       </div>
       {
-        comments.length > 0
+        product.comments.length > 0
           ?
           <Pagination
-            count={Math.ceil(comments.length / COMMENTS_PER_PAGE)}
+            count={Math.ceil(product.comments.length / COMMENTS_PER_PAGE)}
             page={page}
             onChange={(event, value) => setPage(value)}
           />
@@ -46,6 +61,6 @@ const ProductComments = ({ comments }) => {
       }
     </>
   );
-};
+});
 
 export default ProductComments;
