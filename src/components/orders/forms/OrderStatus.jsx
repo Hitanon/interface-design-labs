a3c3d1b430946a9916597234eb5e4adbd7321570
Korablev Danil <ikorablev@gmail.com>
2024-04-11 23:52:09 +0500

    fix

--- src/components/orders/forms/OrderStatus.jsx
+++ src/components/orders/forms/OrderStatus.jsx
@@ -1,7 +1,25 @@
 const OrderStatus = ({ status, isActive }) => {
+  const translateStatus = (status) => {
+    if (status === "CREATED") {
+      return "Создан";
+    }
+    if (status === "ACCEPTED") {
+      return "Принят";
+    }
+    if (status === "PACKED") {
+      return "Упакован";
+    }
+    if (status === "SENT") {
+      return "Отправлен";
+    }
+    if (status === "RECEIVED") {
+      return "Получен";
+    }
+  };
+
   return (
     <div className={isActive ? "status status-active" : "status status-inactive"}>
-      {status}
+      {translateStatus(status)}
     </div>
   );
 };
